5c9874ece251523e0dbf04ce4c795246d5b7ee17
Fabio Falci <fabiofalci@gmail.com>
2017-08-17 18:54:57 +0100

    Quiet ffmpeg and logs

--- ffmpeg/run.sh
+++ ffmpeg/run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
     
+echo Generating trf files...
 find . -type f -name 'V100*.MOV' -print0 | \
     xargs -0 -P 4 -n 1 -I {} \
-    ffmpeg -i {} -b:v 12000k -vf vidstabdetect=shakiness=5:show=1:result={}.trf -f null -
+    ffmpeg -hide_banner -loglevel panic -i {} -b:v 12000k -vf vidstabdetect=shakiness=5:show=1:result={}.trf -f null -
     
+echo Generating st files...
 find . -type f -name 'V100*.MOV' -print0 | \
     xargs -0 -P 4 -n 1 -I {} \
-    ffmpeg -i {} -b:v 12000k -vf vidstabtransform=input={}.trf,unsharp=5:5:0.8:3:3:0.4 {}.st.mov
+    ffmpeg -hide_banner -loglevel panic -i {} -b:v 12000k -vf vidstabtransform=input={}.trf,unsharp=5:5:0.8:3:3:0.4 {}.st.mov
 
+echo Generating input.txt...
 # macos: gfind . -name '*.st.mov' -printf 'file %f\n' | sort > input.txt
 find -name '*.st.mov' -printf "file %f\n" | sort > input.txt
 
-ffmpeg -f concat -i input.txt -c copy out.mov
+echo Concatenating files...
+ffmpeg -hide_banner -loglevel panic -f concat -i input.txt -c copy out.mov
